1c1894ffb06c153d4c1b05e4949d53a85621978c
RehamAbdelmonem <rehamabdelmonem79@gmail.com>
2023-11-04 07:05:00 +0200

    create note list view

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home:NotesView(),
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+      home: NotesView(),
     );
   }
 }
