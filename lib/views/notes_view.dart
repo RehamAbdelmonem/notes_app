7774af2fea6c9e12d451f42bf308937e8b79218f
RehamAbdelmonem <rehamabdelmonem79@gmail.com>
2023-11-04 07:51:18 +0200

    finish bottom notes view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -7,8 +7,22 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: NotesViewBody(),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)),
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              });
+        },
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+      body: const NotesViewBody(),
     );
   }
 }
