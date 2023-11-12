fe1337a34622e1e55716dc3e9e6b4273508bc4f2
RehamAbdelmonem <rehamabdelmonem79@gmail.com>
2023-11-12 07:40:22 +0200

    notes cubit

---
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
+
+part 'notes_state.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+
+  List<NoteModel>? notes;
+  fetchAllNotes() async {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+
+       notesBox.values.toList();
+     
+  }
+}
